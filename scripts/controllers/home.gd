82a473b491404d9522ff57dde7ee0fca532c167d
hesronf2-coder <hesronf2@gmail.com>
2026-05-20 19:23:57 -0300

    Update home.gd / Adicionando comentários para melhor entendimento do código.
    É uma boa prática.

--- scripts/controllers/home.gd
+++ scripts/controllers/home.gd
@@ -6,15 +6,17 @@ func _ready() -> void:
 	pass
 
 
-
+#botão ir para o modo aprender
 func _on_button_aprender_pressed() -> void:
 	$Control/AudioButton.play()
 	get_tree().change_scene_to_file("res://scenes/tela_aprender.tscn")
-	
+
+#botão ir para a o modo desafio	
 func _on_button_desafio_pressed() -> void:
 	$Control/AudioButton.play()
 	get_tree().change_scene_to_file("res://scenes/tela_desafio.tscn")
-	
+
+#botão sair do app
 func _on_button_quit_pressed() -> void:
 	$Control/AudioButton.play()
 	get_tree().quit()
