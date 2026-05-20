7315ffc0a11eb8f5b4a078c2ddb6ea9be4184dea
luanakeyla631-art <luanakeyla631@gmail.com>
2026-05-20 19:52:02 -0300

    Update controllers_tela_aprender.gd - Atualizando comentários

--- scripts/controllers/controllers_tela_aprender.gd
+++ scripts/controllers/controllers_tela_aprender.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+#declarando variáveis
 @export var quiz: QuizTheme
 
 var index: int
@@ -7,6 +8,7 @@ var index: int
 var current_quiz: QuizQuestion:
 	get: return quiz.theme[index]
 
+#trazendo os nós para o código
 @onready var question_text: Label = $Control/QuestionText
 @onready var question_image: TextureRect = $Control/ImageHolder/QuestionImage
 @onready var question_image_2: TextureRect = $Control/ImageHolder/QuestionImage2
@@ -19,7 +21,8 @@ func _ready() -> void:
 	
 func load_quiz() -> void:
 	question_text.text = current_quiz.question_info
-	
+
+#tipo da questão, no mvp vai ser só a função imagem por enquanto, sem vídeo
 	match current_quiz.type:
 		
 			Enum.QuestionType.IMAGE:
@@ -33,7 +36,9 @@ func load_quiz() -> void:
 				question_video.stream = current_quiz.question_video
 				question_audio.stream = current_quiz.question_audio
 				question_video.play()
-	if index == 0:
+
+#condição para mudar o index para a próxima imagem apertando o botão.
+if index == 0:
 		$Control/ButtonPrevious.hide()
 	else:
 		$Control/ButtonPrevious.show()
@@ -42,20 +47,24 @@ func load_quiz() -> void:
 		$Control/ButtonNext.hide()
 	else:
 		$Control/ButtonNext.show()
+
+#função do botão próxima questão
 func _next_question() -> void:
 	index += 1
-	
+
+#função do botão questão anterior	
 func _previous_question() -> void:
 	index -= 1
 
-
+# conectanto botões e aplicando as funções
+#botão anterior
 func _on_button_previous_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	_previous_question()
 	load_quiz()
 	print(index)	
 
-
+#botão próxima
 func _on_button_next_pressed() -> void:
 	
 	_next_question()
@@ -67,18 +76,23 @@ func _on_button_next_pressed() -> void:
 	$Control/ButtonInfoExit.hide()
 	print(index)
 	
-
+#botão play áudio
+#MVP sem áudio por enquanto
 func _on_button_play_audio_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	question_audio.play()
 	$Control/ButtonPlayAudio.hide()
 	$Control/ButtonStopAudio.show()
+
+#botão play áudio
+#MVP sem áudio por enquanto
 func _on_button_stop_audio_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	question_audio.stop()
 	$Control/ButtonStopAudio.hide()
 	$Control/ButtonPlayAudio.show()
 
+#botão para ver o gesto em libras
 func _on_button_info_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	$Control/ImageHolder/QuestionImage.hide()
@@ -86,6 +100,7 @@ func _on_button_info_pressed() -> void:
 	$Control/ButtonInfo.hide()
 	$Control/ButtonInfoExit.show()
 
+#botão para ver as informações culturais novamente
 func _on_button_info_exit_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	$Control/ImageHolder/QuestionImage2.hide()
@@ -93,6 +108,7 @@ func _on_button_info_exit_pressed() -> void:
 	$Control/ButtonInfo.show()
 	$Control/ButtonInfoExit.hide()
 
+#botão ir para home
 func _on_button_home_pressed() -> void:
 	$Control/AudioButtonPress.play()
 	get_tree().change_scene_to_file("res://scenes/tela_home.tscn")
