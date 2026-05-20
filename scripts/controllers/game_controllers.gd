793d7bfaf0cd83c7e05bcdc09c41bf7308fa99e7
jhenniferalves733-maker <jhenniferalves733@gmail.com>
2026-05-20 19:16:52 -0300

    Update game_controllers.gd - adicionando função _next_question()

--- scripts/controllers/game_controllers.gd
+++ scripts/controllers/game_controllers.gd
@@ -47,3 +47,36 @@ func load_quiz() -> void:
 				$Control/Fundoimagem.show()
 				question_video.stream = current_quiz.question_video
 				question_video.play()
+func _buttons_answer(button) -> void:
+		
+	if current_quiz.correct == button.text:
+		button.modulate = color_green
+		
+		correct += 1
+		$AudioCorrect.play()
+		_question_info()		
+		
+	else:
+		button.modulate = color_red
+		$AudioIncorrect.play()
+		await get_tree().create_timer(1.5).timeout
+		_next_question()
+		#$Control/ButtonGoQuestion.show()
+
+func _next_question() -> void:
+	for bt in buttons:
+		bt.pressed.disconnect(_buttons_answer)
+		
+	
+	
+	for bt in buttons:
+		bt.modulate = Color.WHITE
+		
+	question_audio.stop()
+	question_video.stop()
+	question_audio.stream = null
+	question_video.stream = null
+		
+	$Control/ButtonGoQuestion.hide()
+	index += 1
+	load_quiz()
